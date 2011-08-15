207a61076b15aa25e464b71ba700eee3211aa21b
Gordon Anderson <gordon.b.anderson@gmail.com>
2011-08-15 10:03:45 +0700

    First check in of code

---
+++ code/PageCommentEmailNotification.php
@@ -0,0 +1,20 @@
+<?php
+
+class PageCommentEmailNotification extends DataObjectDecorator { 
+	function onAfterWrite() { 
+		parent::onAfterWrite();
+
+	       $email = new Email(); 
+			$email->setTemplate('NewComment'); 
+			$email->setTo(Email::getAdminEmail()); 
+			// $email->addCustomHeader('Reply-To', Member::currentUser()->Email); 
+			$email->setSubject('New Comment ' . str_replace(array("http://", "https://"), array("", ""), Director::absoluteBaseURL())); 
+			$email->populateTemplate(array( 
+				'URL' => Director::absoluteBaseURL() . $this->owner->Parent()->URLSegment, 
+				'PageTitle' => $this->owner->Parent()->Title, 
+				'Comment' => $this->owner->Comment, 
+				'Name' => $this->owner->Name, 
+			)); 
+			$email->send(); 
+		} 
+}
---
+++ templates/Email/NewComment.ss
@@ -0,0 +1,6 @@
+A new comment has been posted:
+
+Title: $PageTitle
+URL: $URL
+Comment: $Comment
+Person who made comment: $Name
