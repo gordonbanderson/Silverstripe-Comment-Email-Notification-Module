ac48ed914c6c653e6114dcf9eefceda5e2f45b32
Gordon Anderson <gordon.b.anderson@gmail.com>
2012-11-27 20:04:39 +0700

    UPGRADE: Configuration for SS3 version

--- _config.php
+++ _config.php
@@ -1,6 +1,6 @@
 <?php
-DataObject::add_extension('PageComment','PageCommentDecorator');
-DataObject::add_extension('PageComment', 'PageCommentEmailNotification');
-Director::addRules(50, array( 'admin/comments//$Action/$ID' => 'CommentAdminExtension', )); 
-CMSMenu::remove_menu_item('CommentAdmin'); 
+//DataObject::add_extension('PageComment','PageCommentDecorator');
+DataObject::add_extension('CommentingController', 'PageCommentEmailNotification');
+//Director::addRules(50, array( 'admin/comments//$Action/$ID' => 'CommentAdminExtension', )); 
+//CMSMenu::remove_menu_item('CommentAdmin'); 
 ?>
