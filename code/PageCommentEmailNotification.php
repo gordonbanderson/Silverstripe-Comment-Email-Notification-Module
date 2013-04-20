bc918799a96cc8ef98ec365febaecf026343c21d
Gordon Anderson <gordon.b.anderson@gmail.com>
2013-04-20 21:21:35 +0700

    ENHANCEMENT: Added direct edit link

--- code/PageCommentEmailNotification.php
+++ code/PageCommentEmailNotification.php
@@ -2,19 +2,20 @@
 
 class PageCommentEmailNotification extends DataExtension {
 	function onAfterPostComment($comment) {
-		$email = new Email();
+		$from = $comment->Email;
+		$to = Email::getAdminEmail();
+		$subject = SiteConfig::current_site_config()->Title.' - ';
+		$subject .= 'New Comment has been posted';
+		$email = new Email( $from, $to, $subject );
 		$email->setTemplate('NewComment');
-		$email->setFrom(Email::getAdminEmail());
-		$email->setTo(Email::getAdminEmail());
-		$email->addCustomHeader('Reply-To', $comment->Email);
-		$email->setSubject('New Comment ' . str_replace(array("http://", "https://"), array("", ""), Director::absoluteBaseURL()));
-
 		$email->populateTemplate(array(
 				'URL' => Director::absoluteBaseURL() . $comment->Link(),
 				'PageTitle' => $comment->getParent()->Title,
 				'Comment' => $comment->Comment,
-				'Name' => $comment->Name
-			));
+				'Name' => $comment->Name,
+				'EditLink' => Director::absoluteBaseURL().'admin/comments/EditForm/field/Comments/item/'.$comment->ID.'/edit'
+		));
 		$email->send();
 	}
 }
+?>
